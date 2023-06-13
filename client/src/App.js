962bc0bc33ba7916e9f3e11ba9abeda9c0a60d82
Juebele <eronuebele@gmail.com>
2023-06-13 17:08:33 -0400

    added homepage and got it working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Home';
 import Login from './components/Login';
 
 import { ApolloClient, InMemoryCache, ApolloProvider, HttpLink } from '@apollo/client';
@@ -11,6 +12,7 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
+      <Home />
       <Login />
     </ApolloProvider>
   );
---
+++ client/src/components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useState } from 'react';
+
+import { useMutation } from '@apollo/client';
+
+function Home() {
+    
+    return (
+        <div>
+            <h1>Hello, I'm a homepage</h1>
+        </div>
+    )
+}
+
+export default Home;
