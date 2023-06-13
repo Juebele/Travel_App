1d7123e9acfd01918149682184b14f2c42b57cbf
Juebele <eronuebele@gmail.com>
2023-06-13 17:43:53 -0400

    centered divs on landing page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 function Home() {
     return (
-        <div>
-            <p>
-                Do you love to travel, but get overwhelmed by all the planning? 
+        <div className="homepage container-fluid">
+            <p className='d-flex justify-content-center'>
+                Do you love to travel, but get overwhelmed by all the planning?
             </p>
-            <h1>
+            <h2 className='d-flex justify-content-center'>
                 Welcome to (insert official name here). Trip planning made easy.
-            </h1>
+            </h2>
             <button className="btn btn-primary">
                 <Link id="getStarted" className="customLink" to={`/login`}>
                     Get Started
