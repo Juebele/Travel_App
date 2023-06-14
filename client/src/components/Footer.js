ffe198e53397c1f3eef53c6eb1eabff480ce8592
Juebele <eronuebele@gmail.com>
2023-06-14 14:12:47 -0400

    got the footer aligned

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,17 +3,21 @@ import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
-        <div className='footer container-fluid justify-content-center row'>
-            <div className='container-fluid col contributors justify-content-center'>
-                <p>Alisa</p>
-                <p>Cole</p>
-                <p>Dowling</p>
-                <p>Bryant</p>
-                <p>James</p>
+        <div className='footer container-fluid row'>
+            <div className='container-fluid col contributors'>
+                <p className='container-fluid row justify-content-start'>Alisa</p>
+                <p className='container-fluid row justify-content-start'>Cole</p>
+                <p className='container-fluid row justify-content-start'>Dowling</p>
+                <p className='container-fluid row justify-content-start'>Bryant</p>
+                <p className='container-fluid row justify-content-start'>James</p>
             </div>
-            <div className='container-fluid col links'>Site Links</div>
+            <div className='container-fluid col links'>
+                <p className='container-fluid row justify-content-center'>Home</p>
+                <p className='container-fluid row justify-content-center'>Sign Up</p>
+                <p className='container-fluid row justify-content-center'>Log In</p>
+            </div>
             <div className='container-fluid col copyright'>
-                <p className='container-fluid col justify-content-right'>Copyright 2023</p>
+                <p className='container-fluid row justify-content-end'>Copyright 2023</p>
             </div>
         </div>
     )
