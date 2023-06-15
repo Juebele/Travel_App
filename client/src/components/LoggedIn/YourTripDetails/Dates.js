98471e6a3af498c7f1a65f60e5777c783363c77b
Cole <122782745+cm-thom@users.noreply.github.com>
2023-06-15 08:42:16 -0400

    added base functionality for Dates component

---
+++ client/src/components/LoggedIn/YourTripDetails/Dates.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Dates() {
+
+    //include logic to get dates by query
+
+    return (
+       <div>
+        <p>Start Date: </p>
+        <p>End Date: </p>
+       </div> 
+    );    
+};
