741c900cf295e62b22a05b7c75d2c6f9a1ef01ca
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-14 23:43:45 -0700

    the total length of the trip in days is now displayed by the component

--- client/src/components/LoggedIn/YourTripDetails/Dates.js
+++ client/src/components/LoggedIn/YourTripDetails/Dates.js
@@ -8,6 +8,14 @@ export default function Dates() {
     // The number in the following array will need to be changed based on which trip the user chooses to see from the home page
     const trip = trips[0];
 
+    const calculateDays = (startDate, endDate) => {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+        const timeDiff = Math.abs(end - start);
+        const days = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+        return days;
+      };
+    
     return (
         <div>
             {loading ? (
@@ -16,8 +24,9 @@ export default function Dates() {
                 <div>Dates haven't been set yet</div>
             ) : (
                 <div>
-                <p>Start Date: {trip.startDate}</p>
-                <p>End Date: {trip.endDate}</p>
+                    <p>Start Date: {trip.startDate}</p>
+                    <p>End Date: {trip.endDate}</p>
+                    <p>Number of Days: {calculateDays(trip.startDate, trip.endDate)}</p>
                 </div>
             )}
         </div>
