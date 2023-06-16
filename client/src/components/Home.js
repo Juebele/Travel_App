0c5463b886e4b9bf3fddb0d02d908b7b9994c150
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-15 23:18:32 -0700

    fixed formatting of trip cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -42,7 +42,7 @@ const Home = () => {
         <div>
           <Navbar />
           <h2 className="d-flex justify-content-center my-4 fw-bold">Your Trips</h2>
-
+          <div className='d-flex justify-content-center'>
             {data ? data.me.trips.map((trip) => {
               return (
                 <div className="card text-center mb-3 col-3">
@@ -56,9 +56,10 @@ const Home = () => {
                   </div>
                 </div>
               ) 
-            }) : null }
-            <h2 className="d-flex justify-content-center my-4 fw-bold">Create a new trip</h2>
-            <CreateTripForm />
+            }): null }
+          </div>
+          <h2 className="d-flex justify-content-center my-4 fw-bold">Create a new trip</h2>
+          <CreateTripForm />
         </div>
       )}
 
