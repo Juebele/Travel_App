75363356ec0142e85d7be33c4682f7c3a9612f0c
blee241 <128437170+blee241@users.noreply.github.com>
2023-06-16 01:30:56 -0700

    added shadow to trip cards

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -46,7 +46,7 @@ const Home = () => {
             {data ? data.me.trips.map((trip) => {
               return (
                 <div className="card text-center mb-3 col-3 mx-1">
-                  <div className="card-body">
+                  <div className="card-body shadow">
                     <h5 className="card-title">{trip.tripName}</h5>
                     <button className="btn btn-primary">
                       <Link to={`/trips/${trip._id}`} style={{ textDecoration: 'none', color: 'white' }}>
