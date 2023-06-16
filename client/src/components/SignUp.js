339b537ffcf14c4d2fcfc2e874aaa01b8857ac36
A Bevers <alisabevers@gmail.com>
2023-06-16 14:39:10 -0400

    styled login and signup forms

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -44,37 +44,39 @@ function SignUp() {
     return (
         <div>
             <Navbar />
-            <form className="login-signup-form row mx-auto col-10 col-md-8 col-lg-6" onSubmit={handleFormSubmit}>
-                <h1 className="formHeader">
-                    Sign up for your next adventure!
-                </h1>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 
-                    name="username"
-                    value={userInfo.username}
-                    onChange={handleInputChange}
-                    required />
-                        <div id="emailHelp" className="form-text"></div>
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"
-                    name="password"
-                    value={userInfo.password}
-                    onChange={handleInputChange}
-                    required />
-                </div>
-                <div>
-                    Already have an account? {`\n`}
-                    <Link to={`/login`}>
-                        Login here!
-                    </Link>
-                </div>
-                <div className='d-flex justify-content-center'>
-                    <button id="submit-login-signup" type="submit" className="btn btn-primary mb-5">Sign Up</button>
-                </div>
-            </form>
+            <div className="login-signup-form-container">
+                <form className="card login-signup-form row mx-auto col-10 col-md-8 col-lg-6 shadow" onSubmit={handleFormSubmit}>
+                    <h1 className="formHeader">
+                        Sign up for your next adventure!
+                    </h1>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Username</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" 
+                        name="username"
+                        value={userInfo.username}
+                        onChange={handleInputChange}
+                        required />
+                            <div id="emailHelp" className="form-text"></div>
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="exampleInputPassword1"
+                        name="password"
+                        value={userInfo.password}
+                        onChange={handleInputChange}
+                        required />
+                    </div>
+                    <div>
+                        Already have an account? {`\n`}
+                        <Link to={`/login`}>
+                            Login here!
+                        </Link>
+                    </div>
+                    <div className='d-flex justify-content-center'>
+                        <button id="submit-login-signup" type="submit" className="btn btn-primary mb-5">Sign Up</button>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
